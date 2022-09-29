eb20b369ed3d45a074bd5ff91afbd430370c3798
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-09-29 18:18:34 +0600

    question ans update

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -4,12 +4,12 @@ import Question from "../Question/Question";
 const Questions = () => {
   const question1 = "1. How does react work?";
   const ans1 =
-    "React reads these objects and uses them to create HTML elements on the virtual DOM, after which it gets synced with the real DOM. So we'll have trees of objects on the virtual DOM and trees of objects on the real DOM. React automatically updates the associated DOM element when we change data on a React element.";
+    "React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser’s DOM.Unlike browser DOM elements, React elements are plain objects and are cheap to create. React DOM takes care of updating the DOM to match the React elements. The reason for this is that JavaScript is very fast and it’s worth keeping a DOM tree in it to speed up its manipulation";
   const question2 = "2. What are the differences between props and state ?";
   const ans2 =
     "Props are used to pass data from one component to another.The state is a local data storage that is local to the component only and cannot be passed to other components.The this.setState property is used to update the state values in the component.";
-  const question3 = "3. What is useEfacet used for other than loading data?";
-  const ans3 = `(1) setting up a subscription (2) manually changing the DOM in React (3) setInterval work`;
+  const question3 = "3. What is useEffect used for other than loading data?";
+  const ans3 = ` Validating input field, live filtering, trigger animation on new array value, update paragraph list on fetched API data update, updating fetched API data to get BTC updated price, setting up a subscription, manually changing the DOM in React, setInterval and other outside work`;
 
   return (
     <div className="my-10 md:w-[90%] mx-auto">
