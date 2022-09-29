5ae827272bd30f27103676cf547740b67b29f98b
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-09-29 12:10:49 +0600

    toast add

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -4,13 +4,13 @@ import Break from "../Break/Break";
 import BreakTime from "../BreakTime/BreakTime";
 import CoderInfo from "../CoderInfo/CoderInfo";
 import WorkTime from "../WorkTime/WorkTime";
+import swal from "sweetalert";
 const Information = ({ workingTime }) => {
   const [timeBreak, setTimeBreak] = useState(0);
   useEffect(() => {
     const bdBreakTime = localStorage.getItem("time");
     setTimeBreak(bdBreakTime);
   }, []);
-
   return (
     <div>
       <CoderInfo></CoderInfo>
@@ -18,7 +18,14 @@ const Information = ({ workingTime }) => {
       <WorkTime workingTime={workingTime}></WorkTime>
       <BreakTime timeBreak={timeBreak}></BreakTime>
       <div className="mt-10">
-        <button className="btn btn-primary w-full">Activity Completed</button>
+        <button
+          onClick={() =>
+            swal("Good job!", "Your Activity Completed!", "success")
+          }
+          className="btn btn-primary w-full"
+        >
+          Activity Completed
+        </button>
       </div>
     </div>
   );
